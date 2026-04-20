7e53276c8d9c2972cfd4859f5f556bd99939eb4b
아m1nhy2uk <m1nhy2uk@gmail.com>
2026-04-20 17:02:33 +0900

    t1~t9, t12~t14 green

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,12 +2,68 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        String[] expressionBits = expression.split(" ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
+        int num2 = Integer.parseInt(expressionBits[2]);
 
-        return num1 + num2;
+        if (expressionBits[1].equals("+")) {
+            if (expressionBits.length == 5) {
+                int num3 = Integer.parseInt(expressionBits[4]);
+                if (expressionBits[1].equals("+") && expressionBits[3].equals("+")) {
+                    return num1 + num2 + num3;
+                } else if (expressionBits[1].equals("-") && expressionBits[3].equals("-")) {
+                    return num1 - num2 - num3;
+                } else if (expressionBits[1].equals("+") && expressionBits[3].equals("-")) {
+                    return num1 + num2 - num3;
+                } else if (expressionBits[1].equals("-") && expressionBits[3].equals("+")) {
+                    return num1 - num2 + num3;
+                }
+            }
+            return num1 + num2;
+        } else if (expressionBits[1].equals("-")) {
+            if (expressionBits.length == 5) {
+                int num3 = Integer.parseInt(expressionBits[4]);
+                if (expressionBits[1].equals("+") && expressionBits[3].equals("+")) {
+                    return num1 + num2 + num3;
+                } else if (expressionBits[1].equals("-") && expressionBits[3].equals("-")) {
+                    return num1 - num2 - num3;
+                } else if (expressionBits[1].equals("+") && expressionBits[3].equals("-")) {
+                    return num1 + num2 - num3;
+                } else if (expressionBits[1].equals("-") && expressionBits[3].equals("+")) {
+                    return num1 - num2 + num3;
+                }
+            }
+            return num1 - num2;
+        } else if (expressionBits[1].equals("*")) {
+            if(expressionBits.length == 5) {
+                int num3 = Integer.parseInt(expressionBits[4]);
+                if (expressionBits[1].equals("*") && expressionBits[3].equals("*")) {
+                    return num1 * num2 * num3;
+                } else if (expressionBits[1].equals("/") && expressionBits[3].equals("/")) {
+                    return num1 / num2 / num3;
+                } else if (expressionBits[1].equals("*") && expressionBits[3].equals("/")) {
+                    return num1 * num2 / num3;
+                } else if (expressionBits[1].equals("/") && expressionBits[3].equals("*")) {
+                    return num1 / num2 * num3;
+                }
+            }
+            return num1 * num2;
+        } else if (expressionBits[1].equals("/")) {
+            if(expressionBits.length == 5) {
+                int num3 = Integer.parseInt(expressionBits[4]);
+                if (expressionBits[1].equals("*") && expressionBits[3].equals("*")) {
+                    return num1 * num2 * num3;
+                } else if (expressionBits[1].equals("/") && expressionBits[3].equals("/")) {
+                    return num1 / num2 / num3;
+                } else if (expressionBits[1].equals("*") && expressionBits[3].equals("/")) {
+                    return num1 * num2 / num3;
+                } else if (expressionBits[1].equals("/") && expressionBits[3].equals("*")) {
+                    return num1 / num2 * num3;
+                }
+            }
+            return num1 / num2;
+        }
+        return 0;
     }
-
 }
