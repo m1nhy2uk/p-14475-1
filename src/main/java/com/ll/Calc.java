0c36b7158d3efd77045268135250385e08ec2feb
아m1nhy2uk <m1nhy2uk@gmail.com>
2026-04-20 21:50:24 +0900

    [feat] 다항식 계산기 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,7 +3,21 @@ package com.ll;
 import java.util.ArrayList;
 
 public class Calc {
+    private static int calculate(int a, String op, int b) {
+        if (op.equals("+")) return a + b;
+        if (op.equals("-")) return a - b;
+        if (op.equals("*")) return a * b;
+        return 0;
+    }
+
     public static int run(String expression) {
+        while (expression.contains("(")) {
+            int start = expression.lastIndexOf("(");
+            int end = expression.indexOf(")", start);
+            String inner = expression.substring(start + 1, end);
+            expression = expression.substring(0, start) + run(inner) + expression.substring(end + 1);
+        }
+
         String[] expressionBits = expression.split(" ");
         ArrayList<Integer> numList = new ArrayList<>();
         ArrayList<String> opList = new ArrayList<>();
@@ -18,27 +32,17 @@ public class Calc {
 
         for (int i = 0; i < opList.size(); i++) {
             if (opList.get(i).equals("*")) {
-                int result = numList.get(i) * numList.get(i + 1);
+                numList.set(i, calculate(numList.get(i), "*", numList.get(i + 1)));
                 numList.remove(i + 1);
-                numList.set(i, result);
                 opList.remove(i);
                 i--;
             }
         }
         for (int i = 0; i < opList.size(); i++) {
-            if (opList.get(i).equals("+")) {
-                int result = numList.get(i) + numList.get(i + 1);
-                numList.set(i, result);
-                numList.remove(i + 1);
-                opList.remove(i);
-                i--;
-            }else if (opList.get(i).equals("-")) {
-                int result = numList.get(i) - numList.get(i + 1);
-                numList.set(i, result);
-                numList.remove(i + 1);
-                opList.remove(i);
-                i--;
-            }
+            numList.set(i, calculate(numList.get(i), opList.get(i), numList.get(i + 1)));
+            numList.remove(i + 1);
+            opList.remove(i);
+            i--;
         }
         return numList.get(0);
     }
